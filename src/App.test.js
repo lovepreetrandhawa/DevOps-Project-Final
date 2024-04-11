d06ca807f268a942d4366e1b29c58cc94d4ddb90
lovepreetrandhawa <157432467+lovepreetrandhawa@users.noreply.github.com>
2024-04-10 18:52:11 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,14 +4,14 @@ import StudentRecords from './app';
 
 test('renders StudentRecords component', () => {
   render(<StudentRecords />);
-  const headingElement = screen.getByText(/Add Student Records/i);
+  const headingElement = screen.getByText(/Student Records/i); // Update text matcher
   expect(headingElement).toBeInTheDocument();
 });
 
 test('renders form inputs', () => {
   render(<StudentRecords />);
-  const nameInput = screen.getByLabelText(/Name:/i);
-  const gradeInput = screen.getByLabelText(/Grade:/i);
+  const nameInput = screen.getByLabelText(/Student Name:/i); // Update text matcher
+  const gradeInput = screen.getByLabelText(/Student Grade:/i); // Update text matcher
   expect(nameInput).toBeInTheDocument();
   expect(gradeInput).toBeInTheDocument();
 });
