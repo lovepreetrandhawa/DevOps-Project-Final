c5a482d3dade43afafd7b2791851d36436151a20
lovepreetrandhawa <157432467+lovepreetrandhawa@users.noreply.github.com>
2024-04-10 18:04:22 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import StudentRecords from './StudentRecords'; // Assuming your component is named StudentRecords
+import StudentRecords from './StudentRecords';
 
 test('renders StudentRecords component', () => {
   render(<StudentRecords />);
-  // Assuming your StudentRecords component renders some content
-  // You should adjust the below assertions based on what your component renders
   const headingElement = screen.getByText(/Add Student Records/i);
   expect(headingElement).toBeInTheDocument();
 });
