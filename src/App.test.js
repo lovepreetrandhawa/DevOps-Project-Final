25e7114d069e4a1f8176b13b95065885bfa422de
lovepreetrandhawa <157432467+lovepreetrandhawa@users.noreply.github.com>
2024-04-10 18:13:51 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import StudentRecords from './StudentRecords';
+import StudentRecords from './app'; // Assuming your component is named app
 
 test('renders StudentRecords component', () => {
   render(<StudentRecords />);
